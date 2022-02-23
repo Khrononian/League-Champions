4d6ec63afe474ce5c3ec0e45db4420f26aa15430
Khrononian <Alv.Barnes@gmail.com>
2022-02-22 22:04:13 -0500

    Readded fetches

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import  sendToChampionPage  from './displayData'
 
-
-// setInterval(() => console.log(championId), 1000)
-
 const loadGridDivs = () => {
     const selectParent = document.querySelector('.contain');
     const getChampionNames = fetch('https://league-of-legends-stats.p.rapidapi.com/champions', {
@@ -12,31 +9,31 @@ const loadGridDivs = () => {
 		    "x-rapidapi-key": "d5c7b459b7msh3709ab41febef03p1d7c75jsn6c10d4ece44d"
         }
     })
-    // const responses = [
-    //     fetch(`https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=0&size=10`, {
-    //         'method': 'GET',
-    //         'headers': {
-    //             "x-rapidapi-host": "league-of-legends-champions.p.rapidapi.com",
-    //             "x-rapidapi-key": "d5c7b459b7msh3709ab41febef03p1d7c75jsn6c10d4ece44d"
-    //         }
-    //     }),
-    //     fetch(`https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=1&size=10`, {
-    //         'method': 'GET',
-    //         'headers': {
-    //             "x-rapidapi-host": "league-of-legends-champions.p.rapidapi.com",
-    //             "x-rapidapi-key": "d5c7b459b7msh3709ab41febef03p1d7c75jsn6c10d4ece44d"
-    //         }
-    //     }),
-    //     fetch(`https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=2&size=10`, {
-    //         'method': 'GET',
-    //         'headers': {
-    //             "x-rapidapi-host": "league-of-legends-champions.p.rapidapi.com",
-    //             "x-rapidapi-key": "d5c7b459b7msh3709ab41febef03p1d7c75jsn6c10d4ece44d"
-    //         }
-    //     }),
-    // ]
-    // const requests = responses.map(response => response.then(getData => getData.json()))
-    // const getChampionImages = Promise.all(requests)
+    const responses = [
+        fetch(`https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=0&size=10`, {
+            'method': 'GET',
+            'headers': {
+                "x-rapidapi-host": "league-of-legends-champions.p.rapidapi.com",
+                "x-rapidapi-key": "d5c7b459b7msh3709ab41febef03p1d7c75jsn6c10d4ece44d"
+            }
+        }),
+        fetch(`https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=1&size=10`, {
+            'method': 'GET',
+            'headers': {
+                "x-rapidapi-host": "league-of-legends-champions.p.rapidapi.com",
+                "x-rapidapi-key": "d5c7b459b7msh3709ab41febef03p1d7c75jsn6c10d4ece44d"
+            }
+        }),
+        fetch(`https://league-of-legends-champions.p.rapidapi.com/champions/en-us?page=2&size=10`, {
+            'method': 'GET',
+            'headers': {
+                "x-rapidapi-host": "league-of-legends-champions.p.rapidapi.com",
+                "x-rapidapi-key": "d5c7b459b7msh3709ab41febef03p1d7c75jsn6c10d4ece44d"
+            }
+        }),
+    ]
+    const requests = responses.map(response => response.then(getData => getData.json()))
+    const getChampionImages = Promise.all(requests)
     
     for (let i = 0; i < 159; i++) {
         const cardDiv = document.createElement('div');
@@ -82,30 +79,30 @@ const loadGridDivs = () => {
         }
     })
 
-    // getChampionImages
-    // .then(data => {
-    //     console.log(data, data[0])
-    //     const getPTags = document.querySelectorAll('p');
-    //     let index = 0;
+    getChampionImages
+    .then(data => {
+        console.log(data, data[0])
+        const getPTags = document.querySelectorAll('p');
+        let index = 0;
 
-    //     const runFirstPromise = (getIndex) => {
-    //         for (let j = 0; j < getPTags.length; j++) {
-    //             for (let i = 0; i < data[getIndex].champions.length; i++) {
-    //                 // getImgTags[i].src = data.champions[champion].node.champion.profile_image.url
-    //                 if (data[getIndex].champions[i].node.champion.profile_image === null) {
-    //                     continue
-    //                 } else if (getPTags[j].innerText == data[getIndex].champions[i].node.champion_name) {
-    //                     console.log('Check all', getPTags[j], data[getIndex].champions[i].node.champion_name)
-    //                     getPTags[j].parentElement.parentElement.querySelector('img').src = data[getIndex].champions[i].node.champion.profile_image.url
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     while (index <= 2) {
-    //         runFirstPromise(index)
-    //         index++
-    //     }
-    // }).catch(error => console.log(error))
+        const runFirstPromise = (getIndex) => {
+            for (let j = 0; j < getPTags.length; j++) {
+                for (let i = 0; i < data[getIndex].champions.length; i++) {
+                    // getImgTags[i].src = data.champions[champion].node.champion.profile_image.url
+                    if (data[getIndex].champions[i].node.champion.profile_image === null) {
+                        continue
+                    } else if (getPTags[j].innerText == data[getIndex].champions[i].node.champion_name) {
+                        console.log('Check all', getPTags[j], data[getIndex].champions[i].node.champion_name)
+                        getPTags[j].parentElement.parentElement.querySelector('img').src = data[getIndex].champions[i].node.champion.profile_image.url
+                    }
+                }
+            }
+        }
+        while (index <= 2) {
+            runFirstPromise(index)
+            index++
+        }
+    }).catch(error => console.log(error))
 
 }
 const gatherData = index => {
@@ -211,9 +208,6 @@ const gatherData = index => {
 
     
 }
-
-
-
 
 window.addEventListener('load', loadGridDivs)
 
