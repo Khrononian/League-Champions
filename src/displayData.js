baed624f4e40da06c37c88724c9c0d63ec17e47a
Khrononian <Alv.Barnes@gmail.com>
2022-04-04 02:00:36 -0400

    Cleaned code

--- src/displayData.js
+++ src/displayData.js
@@ -4,7 +4,6 @@ const champBtn = document.querySelector('.champ-btn')
 const header = document.querySelector('header');
 const nav = document.querySelector('nav');
 const cards = document.querySelector('.champion-cards');
-
 const classHeader = document.querySelector('.header');
 const championHeader = document.querySelector('.champion-header');
 const championAbility = document.querySelector('.champion-abilities');
@@ -13,8 +12,7 @@ const skinBtns = document.querySelectorAll('.skin-btn');
 const abilityBtns = document.querySelectorAll('.ability-icon')
 
 const sendToChampionPage = event => {
-    console.log('RICE', event.target, event.target.id, gatherData(event))
-
+    gatherData(event)
     header.style.display = 'none';
     nav.style.display = 'none';
     cards.style.display = 'none';
@@ -34,6 +32,17 @@ const sendToChampionList = event => {
     championHeader.style.display = 'none'
     championAbility.style.display = 'none'
     championSkin.style.display = 'none'
+
+    for (let i = 0; i < abilityBtns.length; i++) {
+        if (!abilityBtns[0].parentElement.classList.contains('ability-up-animation')
+        && abilityBtns[i].parentElement.classList.contains('ability-up-animation')) {
+            abilityBtns[0].parentElement.classList.toggle('ability-btn')
+            abilityBtns[0].parentElement.classList.toggle('ability-up-animation')
+            abilityBtns[i].parentElement.classList.toggle('ability-up-animation')
+            abilityBtns[i].parentElement.classList.toggle('ability-btn')
+        }
+        changeAbilitySection(document.querySelector('.champion-container').lastElementChild.innerText, event)
+    }
 }
 
 const changeAbilitySection = (index, event) => {
@@ -45,28 +54,27 @@ const changeAbilitySection = (index, event) => {
         }
     }).then(response => response.json())
     .then(data => {
-        console.log(data)
         const champData = data.champion[0]
         const abilityKey = document.querySelector('.ability-info').firstElementChild;
         const abilityName = document.querySelector('.ability-info').querySelector('h4')
         const abilityDesc = document.querySelector('.ability-info').lastElementChild
         const abilityVideo = document.querySelector('video')
 
-
         abilityKey.innerText = event.target.parentElement.id
-        console.log('Data', champData, champData.hasOwnProperty(event.target.id))
+        if (!event.target.id) {
+            abilityKey.innerText = 'PASSIVE';
+            abilityName.innerText = champData['champion_passive']['champion_passive_name']
+            abilityDesc.innerText = champData['champion_passive']['champion_passive_description']
+            return
+        }
         if (champData.hasOwnProperty(event.target.id)) {
             abilityName.innerText = champData[event.target.id][`${event.target.id}_name`];
             abilityDesc.innerText = champData[event.target.id][`${event.target.id}_description`];
             abilityVideo.firstElementChild.src = champData[event.target.id][`${event.target.id}_video_mp4`]
             abilityVideo.lastElementChild.src = champData[event.target.id][`${event.target.id}_video_webm`]
             abilityVideo.load()
-
         }
-
     })
-
-    
 }
 
 skinBtns.forEach(btn => btn.addEventListener('click', event => {
@@ -90,8 +98,13 @@ abilityBtns.forEach(btn => btn.addEventListener('click', event => {
     event.target.parentElement.classList.toggle('ability-up-animation')
     event.target.parentElement.dataset.click = true
 
-    console.log('Placement', changeAbilitySection(championName.innerText, event))
+    changeAbilitySection(championName.innerText, event)
 }))
+
+document.querySelector('.skin-contain').style.height = document.querySelector('.champion-splash').offsetHeight + 'px'
+window.addEventListener('resize', () => {
+    document.querySelector('.skin-contain').style.height = document.querySelector('.champion-splash').offsetHeight + 'px'
+})
 
 champBtn.addEventListener('click', sendToChampionList)
 
